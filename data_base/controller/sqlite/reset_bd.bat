7324a772a768f09848dad5033977c2cc10039420
kakapukakrokodil <131984978+kakapukakrokodil@users.noreply.github.com>
2023-05-01 21:40:46 +0700

    Delete data_base directory

--- data_base/controller/sqlite/reset_bd.bat
+++
@@ -1,2 +0,0 @@
-g++ -I ./include -L ./lib -static reset_bd.cpp -lsqlite3.dll -o reset_bd
-reset_bd
--- data_base/make.bat
+++
@@ -1,2 +0,0 @@
-g++ -I ./controller/sqlite/include -L ./controller/sqlite/lib -static main.cpp -lsqlite3.dll -o main
-main
